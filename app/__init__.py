f16fe3a83c4df6d2f2fbe7df52bf54b9c951d99c
zura1998 <vashalomidze198@gmail.com>
2022-09-03 23:01:31 +0400

    i add some basic stuff

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from app.extensions import db, migrate
+
+
+def create_app(config_file='config.py'):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    register_extensions(app)
+    # register_blueprints(app)
+
+    @app.route('/')
+    def home():
+        return render_template("home.html")
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
+
